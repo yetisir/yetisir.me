9ab44bbe6b96004e24c503d23d807382533b3b8c
M. Yetisir <yetisir@gmail.com>
2020-07-17 08:52:00 -0700

    adds missing component

---
+++ components/Card.vue
@@ -0,0 +1,58 @@
+<template>
+  <vue-glow color="white" :elevation="mouseOver ? 10 : 0">
+    <v-card
+      width="500"
+      :href="url"
+      target="_blank"
+      outlined
+      :loading="loading"
+      @mouseenter="mouseOver = true"
+      @mouseleave="mouseOver = false"
+      :img="image"
+    >
+      <v-card-title :class="[mouseOver ? 'glow' : '']">
+        {{ title }}
+      </v-card-title>
+      <slot />
+      <v-overlay v-if="mouseOver" absolute></v-overlay>
+    </v-card>
+  </vue-glow>
+</template>
+
+<script>
+import VueGlow from 'vue-glow'
+export default {
+  components: {
+    VueGlow,
+  },
+  props: {
+    url: {
+      type: String,
+      default: '',
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      mouseOver: false,
+    }
+  },
+}
+</script>
+<style>
+.v-card__text,
+.v-card__title {
+  word-break: normal;
+}
+</style>
