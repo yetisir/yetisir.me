4942ca07d0614b80203d4925fc2761fd36e1b828
M. Yetisir <yetisir@gmail.com>
2020-07-22 12:29:51 -0700

    updates contacts

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,12 +7,19 @@
     <v-row justify="center" class="ma-6">
       <card title="+1 (858) 336-1186" link="tel:+18583361186"> </card>
     </v-row>
-    <v-row justify="center" class="ma-6">
-      <quote
-        image="/background_8.jpg"
-        quote="If you choose not to decide, you still have made a choice."
-        author="Neil Peart"
-      />
+    <v-row justify="center">
+      <v-card width="500" class="picture"> </v-card>
+    </v-row>
+    <v-row justify="center">
+      <v-card color="transparent">
+        <v-card-title class="font-weight-light">
+          &#8220;If you choose not to decide, you still have made a
+          choice.&#8221;
+        </v-card-title>
+        <v-card-subtitle class="text-right">
+          <em>&mdash;Neil Peart</em>
+        </v-card-subtitle>
+      </v-card>
     </v-row>
   </v-container>
 </template>
@@ -34,3 +41,20 @@ export default {
   },
 }
 </script>
+<style>
+.picture {
+  width: 500px;
+  height: 500px;
+  position: relative;
+  background: url('/background_6.jpg');
+  background-size: 300%;
+  background-position: 50% 50%;
+  mask-image: radial-gradient(
+    circle,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 1) 20%,
+    rgba(0, 0, 0, 0) 55%,
+    rgba(0, 0, 0, 0) 100%
+  );
+}
+</style>
