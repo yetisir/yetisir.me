f2983720376dacf85ec7f6964c6ff371ba85e165
M. Yetisir <yetisir@gmail.com>
2020-12-10 14:47:32 -0800

    adds test site:

---
+++ temp/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <pages />
+  </main>
+</template>
+
+<script>
+import Pages from './components/Pages.vue'
+
+export default {
+  components: {
+    Pages
+  }
+}
+</script>
+
+<style lang="scss"></style>
